d6a70ea2bcd826a9bd4a7170fd0d29b6e17f2c7b
adarsh gurjar <adarsh7470827890@gmail.com>
2024-01-12 18:21:52 +0530

    firstcommit

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require("express")
+const app = express();
+const Routes  = require("./routes/Routes")
+const cors = require('cors')
+require("./config/connection")
+const user = require("./models/HR")
+ const cookieparser = require('cookie-parser')
+// PORT
+const PORT = process.env.PORT || 4000;
+
+const path = require('path')
+
+// app.use(express.static(path.join(__dirname , './client/app/build')))
+console.log(path.join(__dirname ))
+// app.get('*',function(req,res){
+//      res.sendFile(path.join(__dirname , './client/app/build/index.html' ))
+// })
+// middlew
+app.use(cors());
+app.use(cookieparser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api',Routes)
+
+
+
+
+// create server
+
+
+
+
+const start = async()=>{
+
+    try {
+          
+        app.listen(PORT , ()=>{
+ 
+             console.log(`Server Run Sucessfully at PORT: ${PORT}`);
+
+        })
+
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+start();
+  
+       
+
